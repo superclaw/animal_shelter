483437423ddff45f4b6ba0f57677c35c9d4d5563
superclaw <makskirclaw@gmail.com>
2020-08-10 20:29:03 +0700

    Project start

---
+++ build/precache-manifest.29cb2a6ebfa74c21101c4bf7d21f4da8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5283dd0d92945a8f128cf163767d366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26eaba1efe92af28f7b9",
+    "url": "/static/css/main.0e1f87d5.chunk.css"
+  },
+  {
+    "revision": "35d972095d240aa24b69",
+    "url": "/static/js/2.cebdbaec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cebdbaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26eaba1efe92af28f7b9",
+    "url": "/static/js/main.6f256e65.chunk.js"
+  },
+  {
+    "revision": "5d40fe9295d14aa68826",
+    "url": "/static/js/runtime-main.c30d06e6.js"
+  }
+]);
