f2da91f99ae972e0a850eda84cd1a82cbaac79ad
superclaw <makskirclaw@gmail.com>
2020-08-10 23:53:39 +0700

    Path test

---
+++ build/precache-manifest.89c0e7a8227069c274fd1f6e421f3d57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2881f67b2a7ea570b1f4c96deeedac87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9448925c1dfd36db487c",
+    "url": "./static/css/main.345e85b9.chunk.css"
+  },
+  {
+    "revision": "accba64712641164a9c7",
+    "url": "./static/js/2.e112addc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.e112addc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9448925c1dfd36db487c",
+    "url": "./static/js/main.306d2d13.chunk.js"
+  },
+  {
+    "revision": "e47a6cd800b51fbc95ea",
+    "url": "./static/js/runtime-main.f3e8d8b2.js"
+  },
+  {
+    "revision": "5bb3767eb5c56f83a483856d3c90bdcb",
+    "url": "./static/media/background.5bb3767e.jpg"
+  }
+]);
